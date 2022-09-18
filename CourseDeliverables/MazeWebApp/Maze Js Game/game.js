fe3f8b25c8f0f2a7870591e295751d88bc8b2f24
ElieBadari <elie.badari@hotmail.com>
2022-09-18 21:37:46 +0300

    added start function with wall color change

---
+++ CourseDeliverables/MazeWebApp/Maze Js Game/game.js
@@ -0,0 +1,39 @@
+window.onload = () =>{
+
+    const startButton = document.getElementById("start");
+    const walls = document.getElementsByClassName("boundary");
+
+    for(let i = 0; i < walls.length; i++){
+       console.log(walls[i]);
+        walls[i].style.background = "red";
+    }
+
+    startButton.addEventListener("mouseenter", () =>{
+
+
+        for(let i = 0; i < walls.length; i++){
+            console.log(walls[i]);
+             walls[i].style.background = "#eeeeee";
+         }
+
+    });
+
+    for(let i = 0; i< walls.length; i++){
+        walls.addEventListener("mouseenter", () =>{
+
+            for(let i = 0; i < walls.length; i++){
+                console.log(walls[i]);
+                 walls[i].style.background = "red";
+             }
+    
+    
+        });
+    
+
+    }
+    
+
+
+
+
+}
