bcd620bccf50891d25baa1e5b9bace7758701430
ElieBadari <elie.badari@hotmail.com>
2022-09-18 19:27:35 +0300

    added function for eventlistner

--- CourseDeliverables/MazeWebApp/Maze Js Game/login.js
+++ CourseDeliverables/MazeWebApp/Maze Js Game/login.js
@@ -8,7 +8,32 @@ window.onload = () =>{
 
 
 
+    loginButton.addEventListener("click", () =>{
 
+
+        const username = usernameField.value;
+        const password = passwordField.value;
+
+        const user = JSON.parse(storage.getItem(username));
+
+        if(user == null){
+            //user doesnt exist
+            alert("User does not exist");
+
+        }else if (user["password"] == password){
+            //login
+            storage.setItem("currentUser",username);
+            location.assign("game.html")
+        }else{
+            //wrong password
+            alert("Log in credentials are incorrect");
+        }
+
+
+
+
+
+    });
 
 
 
