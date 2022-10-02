5050ae9f098f1ebb78923063992e83c088eb4c9a
ElieBadari <elie.badari@hotmail.com>
2022-10-02 13:08:03 +0300

    added turnhandler

--- CourseDeliverables/TicTacToeWebApp/script.js
+++ CourseDeliverables/TicTacToeWebApp/script.js
@@ -8,7 +8,24 @@ let is_playing = 0; //determines who is playing, 0(false) = red, 1(true) = yello
 const game_state = [-1,-1,-1,-1,-1,-1,-1,-1,-1]; //determines whether a tic has been played or not (inorder to not overrite a play)
 
 
+const turnHandler0 = (e) =>{ 
 
+    if(!is_playing && game_state[0] == -1){ //if it is reds turn and the segment is empty
+
+        is_playing = 0;
+        tics[0].src = "Assets\\red.png";
+
+
+
+    }else{
+        is_playing = 1;
+        tics[0].src = "Assets\\yellow.png";
+    }
+
+
+
+
+}
 
 
 
