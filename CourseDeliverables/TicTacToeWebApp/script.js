d794c93edd2a4c66e9d3d8fc55dab88768ff23b5
ElieBadari <elie.badari@hotmail.com>
2022-10-02 13:18:56 +0300

    added gamestate to stop overwrite + fixed turnhandler

--- CourseDeliverables/TicTacToeWebApp/script.js
+++ CourseDeliverables/TicTacToeWebApp/script.js
@@ -3,30 +3,57 @@ const segments = document.querySelectorAll(".position"); //each img container in
 const reset_btn = document.getElementById("reset-btn");//reset buttn element
 
 
-let is_playing = 0; //determines who is playing, 0(false) = red, 1(true) = yellow
+let is_playing = 0; //determines who is playing, 0 = red, 1 = yellow
 
-const game_state = [-1,-1,-1,-1,-1,-1,-1,-1,-1]; //determines whether a tic has been played or not (inorder to not overrite a play)
+let game_state = [-1,-1,-1,-1,-1,-1,-1,-1,-1]; //determines whether a tic has been played or not (inorder to not overrite a play)
+
+
 
 
 const turnHandler0 = (e) =>{ 
 
-    if(!is_playing && game_state[0] == -1){ //if it is reds turn and the segment is empty
+    tics[0].classList.remove("start");
 
-        is_playing = 0;
-        tics[0].src = "Assets\\red.png";
+    if(game_state[0] == -1){
 
+        game_state[0] = 1;
 
-
-    }else{
-        is_playing = 1;
-        tics[0].src = "Assets\\yellow.png";
+        if(is_playing == 0){ 
+            is_playing = 1;
+            tics[0].src = "Assets\\red.png";
+        }else{
+            is_playing = 0;
+            tics[0].src = "Assets\\yellow.png";
+        }
+        
     }
-
-
-
-
 }
 
+const turnHandler1 = (e) =>{ 
+
+     tics[1].classList.remove("start");
+
+    if(game_state[1] == -1){
+        
+        game_state[1] = 1;
+
+        if(is_playing == 0){ 
+            is_playing = 1;
+            tics[1].src = "Assets\\red.png";
+        }else{
+            is_playing = 0;
+            tics[1].src = "Assets\\yellow.png";
+        }
+        
+    }
+}
+
+
+tics.forEach(t => t.classList.add("start"));
+
+segments[0].addEventListener("click",turnHandler0);
+
+segments[1].addEventListener("click",turnHandler1);
 
 
 
